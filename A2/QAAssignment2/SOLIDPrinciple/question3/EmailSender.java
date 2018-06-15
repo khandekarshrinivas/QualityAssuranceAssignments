62aa96c3e218ca83f35a12d85d21a3e361749368
Shrinivas Khandekar <sh258831@dal.ca>
2018-06-15 15:50:42 -0300

    Question 3

--- A2/QAAssignment2/SOLIDPrinciple/question3/EmailSender.java
+++ A2/QAAssignment2/SOLIDPrinciple/question3/EmailSender.java
@@ -1,9 +1,24 @@
 package question3;
 
-public class EmailSender
-{
-	public void SendEmail(String emailAddress, String subject, String message)
-	{
+import java.util.ArrayList;
+
+public class EmailSender {
+
+	public void SendToEmail(String emailAddress, ProfitReport profitReport) {
+		try {
+			ArrayList<String> reportData = profitReport.getReportData();
+			StringBuilder builder = new StringBuilder();
+			for (int i = 0; i < reportData.size(); i++) {
+				builder.append(reportData.get(i) + "\n");
+			}
+			EmailSender sender = new EmailSender();
+			sender.SendEmail(emailAddress, "Profit Report!", builder.toString());
+		} catch (Exception e) {
+			System.out.println("Yipes internet must be down!");
+		}
+	}
+
+	public void SendEmail(String emailAddress, String subject, String message) {
 		// I'm pretending to send an email!
 		System.out.println("To: " + emailAddress);
 		System.out.println("Subject: " + subject);
--- A2/QAAssignment2/SOLIDPrinciple/question3/ProfitReport.java
+++ A2/QAAssignment2/SOLIDPrinciple/question3/ProfitReport.java
@@ -1,32 +1,15 @@
 package question3;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import java.util.ArrayList;
 
-import javax.print.Doc;
-import javax.print.DocFlavor;
-import javax.print.DocPrintJob;
-import javax.print.PrintService;
-import javax.print.PrintServiceLookup;
-import javax.print.SimpleDoc;
-import javax.print.attribute.HashPrintRequestAttributeSet;
-import javax.print.attribute.PrintRequestAttributeSet;
-import javax.print.attribute.standard.Copies;
-import javax.print.event.PrintJobAdapter;
-import javax.print.event.PrintJobEvent;
-
-public class ProfitReport
-{
+public class ProfitReport {
 	private ArrayList<String> reportData;
 
-	public ProfitReport()
-	{
+	public ProfitReport() {
 		reportData = new ArrayList<String>();
 	}
 
-	public void CreateReport()
-	{
+	public void CreateReport() {
 		reportData.add("Canada made $100000");
 		reportData.add("Mexico made $1007700");
 		reportData.add("Russia made $10009940");
@@ -38,64 +21,11 @@ public class ProfitReport
 		reportData.add("Chile made $1000400");
 	}
 
-	public void SendToPrinter()
-	{
-		try
-		{
-	    	String defaultPrinter = PrintServiceLookup.lookupDefaultPrintService().getName();
-	    	PrintService service = PrintServiceLookup.lookupDefaultPrintService();
-	    	StringBuilder builder = new StringBuilder();
-	    	for (int i = 0; i < reportData.size(); i++)
-	    	{
-	    		builder.append(reportData.get(i) + "\f");
-	    	}
-	    	InputStream is = new ByteArrayInputStream(builder.toString().getBytes("UTF8"));
-	    	PrintRequestAttributeSet pras = new HashPrintRequestAttributeSet();
-	    	pras.add(new Copies(1));
-	    	DocFlavor flavor = DocFlavor.INPUT_STREAM.AUTOSENSE;
-	    	Doc doc = new SimpleDoc(is, flavor, null);
-	    	DocPrintJob job = service.createPrintJob();
-	    	job.addPrintJobListener(new PrintJobAdapter() {
-	      		public void printJobCanceled(PrintJobEvent pje) {
-	        		allDone();
-	      		}
-	      		public void printJobCompleted(PrintJobEvent pje) {
-	        		allDone();
-	      		}
-	      		public void printJobFailed(PrintJobEvent pje) {
-	        		allDone();
-	      		}	
-	      		public void printJobNoMoreEvents(PrintJobEvent pje) {
-	        		allDone();
-	      		}
-	      		void allDone() {
-	          		System.out.println("Printing done ...");
-	      		}
-	    	});
-	    	job.print(doc, pras);
-	    	is.close();
-		}
-		catch (Exception e)
-		{
-			System.out.println("Printing failed or something!");
-		}
+	public ArrayList<String> getReportData() {
+		return reportData;
 	}
 
-	public void SendToEmail(String emailAddress)
-	{
-		try
-		{
-	    	StringBuilder builder = new StringBuilder();
-	    	for (int i = 0; i < reportData.size(); i++)
-	    	{
-	    		builder.append(reportData.get(i) + "\n");
-	    	}
-	    	EmailSender sender = new EmailSender();
-	    	sender.SendEmail(emailAddress, "Profit Report!", builder.toString());
-		}
-		catch (Exception e)
-		{
-			System.out.println("Yipes internet must be down!");
-		}
+	public void setReportData(ArrayList<String> reportData) {
+		this.reportData = reportData;
 	}
 }
