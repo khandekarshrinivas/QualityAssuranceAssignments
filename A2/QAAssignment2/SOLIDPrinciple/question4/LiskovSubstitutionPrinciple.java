f322f45fcfd5abb7040dca782c85b65e628a1f3f
Shrinivas Khandekar <sh258831@dal.ca>
2018-06-15 21:49:19 -0300

    Updated LSP Principle

--- A2/QAAssignment2/SOLIDPrinciple/question4/LiskovSubstitutionPrinciple.java
+++ A2/QAAssignment2/SOLIDPrinciple/question4/LiskovSubstitutionPrinciple.java
@@ -3,7 +3,13 @@ package question4;
 public class LiskovSubstitutionPrinciple {
 	public static void main(String[] args) {
 
-		BankAccount account = new USDollarAccount();
+		BankAccount account = null;
+		
+		account = new BankAccount();
+		account.Credit(12);
+		System.out.println(account.GetBalance());
+		
+		account = new USDollarAccount();
 		account.Credit(12);
 		System.out.println(account.GetBalance());
 	}
